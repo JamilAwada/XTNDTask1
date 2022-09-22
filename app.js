060ff6c674daef3ed037fa125c1a96a18c887c52
Jamil Awada <jamilawada2002@gmail.com>
2022-09-22 15:45:16 +0300

    made slight UI adjustments

---
+++ app.js
@@ -0,0 +1,9 @@
+// DOM Elements
+
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const amountInput = document.querySelector("#amount");
+
+const submitButton = document.querySelector("submitButton");
+const clearButton = document.querySelector("clearButton");
+
